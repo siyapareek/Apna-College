04954dfe2ac40604be926f3f59a73cc241d30183
siyapareek <93838945+siyapareek@users.noreply.github.com>
2024-05-18 22:58:00 +0530

    Add files via upload

---
+++ Pattern_7.java
@@ -0,0 +1,17 @@
+// 12345
+// 1234
+// 123
+// 12
+// 1
+public class Pattern_7 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
